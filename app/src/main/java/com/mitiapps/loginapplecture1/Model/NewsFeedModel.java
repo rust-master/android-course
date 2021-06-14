3f0a028bc797c4d9b3cbb264e6cd35674688fed7
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-06-14 17:17:49 +0500

    news-feed

---
+++ app/src/main/java/com/mitiapps/loginapplecture1/Model/NewsFeedModel.java
@@ -0,0 +1,20 @@
+package com.mitiapps.loginapplecture1.Model;
+
+public class NewsFeedModel {
+    String image;
+
+    public NewsFeedModel() {
+    }
+
+    public NewsFeedModel(String image) {
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
