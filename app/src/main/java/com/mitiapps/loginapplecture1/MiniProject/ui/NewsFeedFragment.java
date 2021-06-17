c54a176d0f81a43cbde412265c54fc582a61afce
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-06-17 17:40:44 +0500

    lecture-16

--- app/src/main/java/com/mitiapps/loginapplecture1/MiniProject/ui/NewsFeedFragment.java
+++ app/src/main/java/com/mitiapps/loginapplecture1/MiniProject/ui/NewsFeedFragment.java
@@ -3,64 +3,70 @@ package com.mitiapps.loginapplecture1.MiniProject.ui;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.mitiapps.loginapplecture1.Model.NewsFeedModel;
 import com.mitiapps.loginapplecture1.R;
+import com.mitiapps.loginapplecture1.ViewHolder.NewsFeedViewHolder;
+import com.squareup.picasso.Picasso;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NewsFeedFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class NewsFeedFragment extends Fragment {
+    
+    View rootView;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    FirebaseDatabase db;
+    DatabaseReference dbRef;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public NewsFeedFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NewsFeedFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NewsFeedFragment newInstance(String param1, String param2) {
-        NewsFeedFragment fragment = new NewsFeedFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    RecyclerView recyclerView;
+    LinearLayoutManager layoutManager;
+    FirebaseRecyclerAdapter<NewsFeedModel, NewsFeedViewHolder> adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news_feed2, container, false);
+        rootView =  inflater.inflate(R.layout.fragment_news_feed2, container, false);
+
+
+        db =  FirebaseDatabase.getInstance();
+        dbRef = db.getReference("NewsFeed");
+
+        recyclerView = rootView.findViewById(R.id.newsFeedFrgRecler);
+        layoutManager = new LinearLayoutManager(getContext());
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        loadNewsFeed();
+
+
+        return rootView;
+    }
+
+    private void loadNewsFeed() {
+        adapter = new FirebaseRecyclerAdapter<NewsFeedModel, NewsFeedViewHolder>(
+                NewsFeedModel.class,
+                R.layout.news_feed_layout,
+                NewsFeedViewHolder.class,
+                dbRef
+        ) {
+            @Override
+            protected void populateViewHolder(NewsFeedViewHolder viewHolder, NewsFeedModel model, int position) {
+
+                Picasso.with(getContext()).load(model.getImage()).into(viewHolder.imageView);
+                viewHolder.newsText.setText(model.getNewstext());
+            }
+        };
+        adapter.notifyDataSetChanged();
+        recyclerView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/mitiapps/loginapplecture1/ViewHolder/NewsFeedViewHolder.java
+++ app/src/main/java/com/mitiapps/loginapplecture1/ViewHolder/NewsFeedViewHolder.java
@@ -2,6 +2,7 @@ package com.mitiapps.loginapplecture1.ViewHolder;
 
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,9 +12,11 @@ import com.mitiapps.loginapplecture1.R;
 public class NewsFeedViewHolder  extends RecyclerView.ViewHolder  {
 
     public ImageView imageView;
+    public TextView newsText;
 
     public NewsFeedViewHolder(@NonNull View itemView) {
         super(itemView);
          imageView = itemView.findViewById(R.id.imageNews);
+        newsText = itemView.findViewById(R.id.newsText);
     }
 }
